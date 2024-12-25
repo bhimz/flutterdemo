97fd77ada339b58db8c16fc67e0f922690915420
Aria Bima <bhimz2001@gmail.com>
2024-12-25 12:40:48 +0700

    create meal list items

--- lib/widget/category_item.dart
+++ lib/widget/category_item.dart
@@ -10,27 +10,28 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () {
-          onTap(context, category);
-        },
-        splashColor: Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(16),
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                category.color.withValues(alpha: 0.55),
-                category.color.withValues(alpha: 0.9),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+      onTap: () {
+        onTap(context, category);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.55),
+              category.color.withValues(alpha: 0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          child: Text(category.title,
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  )),
-        ));
+        ),
+        child: Text(category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                )),
+      ),
+    );
   }
 }
