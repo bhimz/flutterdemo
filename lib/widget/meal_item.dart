e37d9f2f44ba6254179692a4e63bc401a9b63da5
Aria Bima <bhimz2001@gmail.com>
2024-12-25 12:58:04 +0700

    add meals traits

--- lib/widget/meal_item.dart
+++ lib/widget/meal_item.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/model/meal.dart';
+import 'package:mealsapp/widget/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+
+  String get complexityText {
+    switch (meal.complexity) {
+      case Complexity.simple:
+        return 'Simple';
+      case Complexity.challenging:
+        return 'Challenging';
+      case Complexity.hard:
+        return 'Hard';
+    }
+  }
+
+  String get affordabilityText {
+    switch (meal.affordability) {
+      case Affordability.affordable:
+        return 'Affordable';
+      case Affordability.pricey:
+        return 'Pricey';
+      case Affordability.luxurious:
+        return 'Luxurious';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +72,18 @@ class MealItem extends StatelessWidget {
                         color: Colors.white,
                       ),
                     ),
-                    const SizedBox(height: 12),
+                    const SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        MealItemTrait(icon: Icons.schedule, label: "${meal.duration} min"),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12),
+                        MealItemTrait(icon: Icons.money, label: affordabilityText),
+
+                      ],
+                    ),
                   ],
                 ),
               ),
